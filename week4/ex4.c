e7a393086e375d4b67e67e8f38276ba451d38853
p50000 <rentacat45@gmail.com>
2021-09-15 00:58:50 +0300

    week4

---
+++ week4/ex4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+char **parsed_line(char* line)
+{
+    char *command = strtok(line, " ");
+    char **commands = malloc(sizeof(char *));
+    int size = 1;
+    while (command && command[0] != '\n')
+    {
+        commands[size - 1] = command;
+        size++;
+        command = strtok(NULL, " ");
+        commands = realloc(commands, sizeof(char *) * size);
+    }
+
+    if (commands[size - 2][strlen(commands[size - 2]) - 1] == '\n')
+    {
+        commands[size - 2][strlen(commands[size - 2]) - 1] = '\0';
+    }
+    commands[size] = NULL;
+    return commands;
+}
+
+int main()
+{
+    while(1) 
+    {
+        printf(">");
+        char *line = NULL;
+        size_t n = 0;
+        getline(&line, &n, stdin);
+        char **argv = parsed_line(line);
+        //printf("!!%s!!", argv[0]);
+
+
+        int pid = fork();
+        if (pid == 0) 
+        {
+            execvp(argv[0], argv);
+            return 0;
+        }
+        else waitpid(pid, NULL, 0);
+    }
+}
