9dc8b1b6823475d66b4c1911575014ca106a79fa
p50000 <rentacat45@gmail.com>
2021-10-06 00:00:35 +0300

    week7

--- week7/ex5.c
+++ week7/ex5.c
@@ -5,7 +5,7 @@ int main () {
 	char **s = (char **)malloc(sizeof(char*));
 	char foo [] = "Hello World ";
 	*s = foo;
-	printf ("s is %s\n" ,s);
+	printf ("s is %p\n" ,s);
 	s[0] = foo;
 	printf("s[0] is %s\n", s[0]);
 	return 0;
